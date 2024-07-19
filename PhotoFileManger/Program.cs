8c378b3414b9a3312e8e7d5d8252335138d8d6ae
Crazycatwastaken <maxrasmussen4@gmail.com>
2024-07-19 20:30:58 +1000

    Returning bools for if the year and month folder exist

--- PhotoFileManger/Program.cs
+++ PhotoFileManger/Program.cs
@@ -1,25 +1,87 @@
-﻿using System.Collections.Generic; 
+﻿using System.Collections.Generic;
+using System.Text;
 
 
 class Program
 {
-  
+    class folderPath
+    {
+        public string Path { get; set; }
+        private string currertYear = DateTime.UtcNow.ToString("yyyy");
+        private string currentMonth = DateTime.UtcNow.ToString("MMMM");
+        
+        public folderPath(string path)
+        {
+            Path = path;
+        }
+        
+        
+        public bool CheckYear()
+        {
+            if (Directory.Exists($"{Path}\\{currertYear}"))
+            {
+                Console.WriteLine("Year already exists, creating folder inside directory");
+                return true;
+
+            }
+            return false;
+        }
+        public bool CheckMonth()
+        {
+            if (Directory.Exists($"{Path}\\{currertYear}\\{currentMonth}"))
+            {
+                Console.WriteLine("Month already exists, creating folder inside directory");
+                return true;
+
+            }
+            return false;
+        }
+        
+
+        
+        
+        
+    }
+
     static void Main(string[] args)
     {
-        Dictionary<int, string> dictionary_name = new Dictionary<int, string>();
-        dictionary_name.Add(123, "Test");
-        Console.WriteLine(dictionary_name[123]);
+        folderPath path = new folderPath(@"C:\Users\maxra\Documents\PhotoApplicationTesting");
+        bool monthExists = path.CheckMonth();
+        bool yearExists = path.CheckYear();
+        
+        Console.WriteLine($"{monthExists} and {yearExists}");
 
-        string path = @"C:\Users\maxra\Documents\PhotoApplicationTesting";
+        //
+        // Console.Write("What folder name would you like to create");
+        // string folderName = Console.ReadLine();
+        
 
-        Console.WriteLine(DateTime.Today);
 
-        Console.Write("What folder name would you like to create");
-        string FolderName = Console.ReadLine();
-        Directory.CreateDirectory($"{path}\\{FolderName}");
+
 
 
         
         Console.ReadLine();
     }
+
+    // static void CreateFolder(string path, string folderName)
+    // {
+    //     string currentMonth = DateTime.UtcNow.ToString("MMMM");
+    //     string currertYear = DateTime.UtcNow.ToString("YYYY");
+    //
+    //     if (Directory.Exists($"{path}\\{currentMonth}"))
+    //     {
+    //         Console.WriteLine("Month already exists, creating folder inside directory");
+    //         Directory.CreateDirectory($"{path}\\{currentMonth}\\{folderName}");
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Creating current month folder and folder inside directory");
+    //         Directory.CreateDirectory($"{path}\\{currentMonth}");
+    //         Directory.CreateDirectory($"{path}\\{currentMonth}\\{folderName}");
+    //     }
+    //     // Directory.CreateDirectory($"{path}\\{folderName}");
+    //     
+    // }
+
 }
