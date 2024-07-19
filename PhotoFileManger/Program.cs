7f3e1cf99d01146eca3758c86c702ba8d2e77aee
Crazycatwastaken <maxrasmussen4@gmail.com>
2024-07-19 19:55:49 +1000

    Working Making folders and date time.

--- PhotoFileManger/Program.cs
+++ PhotoFileManger/Program.cs
@@ -1,7 +1,25 @@
-﻿class Program
+﻿using System.Collections.Generic; 
+
+
+class Program
 {
+  
     static void Main(string[] args)
     {
+        Dictionary<int, string> dictionary_name = new Dictionary<int, string>();
+        dictionary_name.Add(123, "Test");
+        Console.WriteLine(dictionary_name[123]);
+
+        string path = @"C:\Users\maxra\Documents\PhotoApplicationTesting";
+
+        Console.WriteLine(DateTime.Today);
+
+        Console.Write("What folder name would you like to create");
+        string FolderName = Console.ReadLine();
+        Directory.CreateDirectory($"{path}\\{FolderName}");
+
+
+        
         Console.ReadLine();
     }
 }
